fc6601271e95dc52dbde0996efe2314eae70edac
aalduhaim <aaldhem@gmail.com>
2024-01-30 10:13:17 +0300

    add routing pathes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component: HomeComponent},
+  {path:"list", component: ReservationListComponent},
+  {path:"form", component: ReservationFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/module/reservation.ts
@@ -0,0 +1,7 @@
+export interface Reservation {
+    id: string,
+    checkInDate: Date,
+    checkOutDate: Date,
+    gusteName: string,
+    roomNumber: number
+}
